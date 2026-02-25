a075738d5cafd0ab29a9bbea5b92a758371a525f
s587427 <s587427@gmail.com>
2026-02-25 13:40:54 +0800

    chore: project init

---
+++ prettier.config.ts
@@ -0,0 +1,20 @@
+import { type Config } from "prettier";
+
+const config: Config = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false, // 用空白，不用 tab 字元
+
+  semi: false, // 分號
+  singleQuote: false,
+  jsxSingleQuote: false,
+
+  trailingComma: "es5", // 尾逗號
+  bracketSpacing: true, // 物件大括號內加空白
+  bracketSameLine: false, // JSX 的 > 不跟最後一個 prop 同行
+
+  arrowParens: "always", // 單一參數也加括號
+  endOfLine: "lf", // 統一使用 LF 換行
+};
+
+export default config;
